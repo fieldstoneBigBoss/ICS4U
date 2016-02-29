236146777d79c79cccc414baa271b71c99c312ff
jason huang <1157977655@qq.com>
2016-02-29 10:01:24 -0500

    2016-02-29 commit

---
+++ a1.py
@@ -0,0 +1,37 @@
+
+#1
+def helloWorld():
+    print("Hello world!")
+helloWorld()
+
+
+#2
+def greet(x):
+    print("Hello",x,"!")
+greet("john")
+
+
+#3
+def greet2():
+    a=input("What's your name?")
+    print("Hello",a,"!")
+greet2()
+
+
+#4
+def plus1(num):
+    return num+1
+plus1()
+
+
+#5
+def max3(a,b,c):
+    return(max(a,b,c))
+
+#6
+def printN():
+    print("1 \nn")
+printN()
+
+#7
+#def sumN():
